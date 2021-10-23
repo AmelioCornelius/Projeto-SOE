6c0be64ec14b72f4193600b483ad820fcb095512
AmelioCornelius <umadecisaohorrorosa@gmail.com>
2021-10-22 22:12:56 -0300

    comentários
    Adição de comentários aos códigos

--- código-fonte/mlx90614.c
+++ código-fonte/mlx90614.c
@@ -1,6 +1,7 @@
-//gcc mlx90614.c -o mlx90614 -l bcm2835
+//gcc mlx90614.c -o temp -l bcm2835
+//sudo -o temp
 #include <stdio.h>
-#include <bcm2835.h>
+#include <bcm2835.h> // biblioteca necessária para o sensor
 #include <stdlib.h>
 #include <fcntl.h>
 #include <string.h>
@@ -15,31 +16,31 @@ int main(int argc, char **argv)
 {
     unsigned char buf[6];
     unsigned char i, reg;
-    double temp = 0,calc = 0, skytemp, atemp;
+    double temp = 0,calc = 0, ctemp, atemp;
     bcm2835_init();
     bcm2835_i2c_begin();
-    bcm2835_i2c_set_baudrate(25000);
+    bcm2835_i2c_set_baudrate(25000); // taxa de transmissão
     // endereço
     bcm2835_i2c_setSlaveAddress(0x5a);
  
     printf("\nMedindo!!\n");
  
-    calc = 0;
+    calc = 0; // variável auxiliar
     reg = 7;
  
     for(i=0;i<AVG;i++)
 	{
-        bcm2835_i2c_begin();
+        bcm2835_i2c_begin(); // início da comunicação i2c
         bcm2835_i2c_write (&reg, 1);
         bcm2835_i2c_read_register_rs(&reg,&buf[0],3);
         temp = (double) (((buf[1]) << 8) + buf[0]);
         temp = (temp * 0.02)-0.01;
-        temp = temp - 268.1;
+        temp = temp - 268.1; // conversão para celsius e ajustes
         calc+=temp;
         sleep(1);
     }
  
-    skytemp = calc/AVG;
+    ctemp = calc/AVG; // temperatura corporal média
     calc = 0;
     reg = 6;
  
@@ -54,16 +55,17 @@ int main(int argc, char **argv)
         sleep(1);
     }
  
-    atemp = calc/AVG;
+    atemp = calc/AVG; // temperatura ambiente média
  
     printf("Temperatura Ambiente = %04.2f\n", atemp);
-    printf("Temperatura Corporal = %04.2f\n", skytemp);
- 
-    if(35.0 <skytemp< 37.7)
+    printf("Temperatura Corporal = %04.2f\n", ctemp);
+    
+    // diagnostico conforme o valor da temperatura corporal 
+    if(35.0 <ctemp< 37.7)
         printf("Temperatura normal\n");
-    else if (skytemp <35.0)
+    else if (ctemp <35.0)
         printf("Hipotermia\n");
-    else if (skytemp > 37.7)
+    else if (ctemp > 37.7)
         printf("Febre\n");
     return 0;
 }
